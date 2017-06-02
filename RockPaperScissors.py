4cbd9367c9a223c089d89b3e2649c2f3dd750129
Alex Hopkins <hopkinsalex@knights.ucf.edu>
2017-06-02 02:09:57 -0400

    Initial commit for RockPaperScissors, a game with AI

---
+++ RockPaperScissors.py
@@ -0,0 +1,18 @@
+# Created by alex 
+# on 6/2/17
+
+"""
+Rock Paper Scissors with AI
+"""
+
+
+def mainGame():
+	"""
+	
+	Main game loop, type rock, paper, or scissors to play the CPU. Type quit to stop playing.
+	
+	"""
+
+
+if __name__ == '__main__':
+	mainGame()
